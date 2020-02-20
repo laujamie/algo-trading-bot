e80438f6ea3f4d65b09425f9428773da037446a5
laujamie <36782341+laujamie@users.noreply.github.com>
2020-02-20 05:57:27 +0100

    Add completed bot and add credit for algorithm

---
+++ README.md
@@ -0,0 +1,16 @@
+# Algorithmic Trading Bot
+
+An algorithmic trading bot created in Python to trade distressed stocks and **QQQ**.
+
+Algorithm used here can be found on ![Medium](https://medium.com/automation-generation/how-to-trade-distressed-stocks-using-free-apis-dd7f43e9be33).
+
+## Usage
+
+Assuming you are on a UNIX system,
+
+```shell
+pipenv install
+pipenv shell
+python3 src/scraper/main.py
+python3 src/bot/main.py
+```
---
+++ src/bot/main.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import json
+import alpaca_trade_api as tradeapi
+
+base_url = 'https://paper-api.alpaca.markets'
+tenquant_base_url = 'https://api.tenquant.io'
+
+if __name__ == '__main__':
+    tickers = []
+
+    with open('tickers.txt', 'r+') as f:
+        tickers = f.read().splitlines()
+
+    tenquant_api = os.environ['TENQUANT_API']
+
+    key = os.environ['ALPACA_API']
+    secret = os.environ['ALPACA_SECRET']
+    api = tradeapi.REST(key, secret, base_url, api_version='v2')
+    clock = api.get_clock()
+
+    if not clock.is_open:
+        print('Market closed, exiting now...')
+        exit
+
+    all_stocks_data = {}
+    for ticker in tickers:
+        params = {'ticker': ticker, 'key': tenquant_api}
+        req_json = requests.get(tenquant_base_url + '/data',
+                                params=params).content
+        try:
+            stock_data = dict(json.loads(req_json))
+        except:
+            continue
+        if 'error' in stock_data.keys():
+            continue
+
+        current_assets = stock_data['currentassets']
+        current_liabilities = stock_data['currentliabilities']
+
+        current_ratio = current_assets / current_liabilities
+
+        all_stocks_data[ticker] = current_ratio
+
+    all_stocks_data = sorted(all_stocks_data.items(), key=lambda x: x[1])
+
+    shorts = all_stocks_data[len(all_stocks_data) // 5 + 1]
+    print('Shorts:')
+    print(shorts)
+
+    weightings = {}
+    portfolio_value = float(account.portfolio_value)
+    for stock, current_ratio in shorts:
+        price = api.get_barset(stock, "minute", 1)[stock][0].c
+        weightings[stock] = -((portfolio_value / 2) / len(shorts)) / price
+    qqq_price = api.get_barset('QQQ', "minute", 1)['QQQ'][0].c
+    weightings['QQQ'] = (portfolio_value / 2) / qqq_price
+
+    for short_stock in weightings.keys():
+        qty = int(weightings[short_stock])
+        if qty < 1:
+            side = 'sell'
+        else:
+            side = 'buy'
+        qty = abs(qty)
+        # place orders
+        try:
+            api.submit_order(short_stock, qty, side, "market", "day")
+            print("Market order of | " + str(qty) + " " + short_stock + " " +
+                  side + " | completed.")
+        except:
+            print("Order of | " + str(qty) + " " + short_stock + " " + side +
+                  " | did not go through.")
